26a3bf89132583c6a55da42374f94a0ec65bbc21
blxtok <89456941+blxtok@users.noreply.github.com>
2022-01-16 14:57:17 +0300

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 2629642516 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/blxtok/Xhan-Ware/main/games/bitcoinminer.lua")()
+else
+    game.Players.LocalPlayer:Kick("Game not supported.")
+end

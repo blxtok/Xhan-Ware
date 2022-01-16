e24c51d31363deda15f9d0171ad0537f42f8062d
blxtok <89456941+blxtok@users.noreply.github.com>
2022-01-16 15:06:54 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 2629642516 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/blxtok/Xhan-Ware/main/games/bitcoinminer.lua")()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/blxtok/Xhan-Ware/main/games/bitcoinminer.lua"))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/blxtok/Xhan-Ware/main/main.lua")()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/blxtok/Xhan-Ware/main/main.lua"))()
 end
